5d511ee33e4b5c8ec9ebe5cdc822e494778e6295
zealshah96 <Zealshah96@gmail.com>
2018-09-12 12:03:26 +0530

    Dyanmic code/text  injector with dynamic config file

---
+++ codeinjectore.js
@@ -0,0 +1,50 @@
+"use strict";
+exports.__esModule = true;
+var fs_1 = require("fs");
+var codeinjector = /** @class */ (function () {
+    function codeinjector() {
+    }
+    codeinjector.prototype.main = function (filename, filedirectorypath, className, CodeToChange, CodeToIdentifie) {
+        var filePath = filedirectorypath + '\\' + filename;
+        var className=className;
+        var CodeToChange=CodeToChange;
+        var CodeToIdentifie=CodeToIdentifie;
+        console.log(filePath);
+        var stringToAdd = "" + CodeToChange;
+        ///File Read Will Start From here 
+        var data=fs_1.readFileSync(filePath, { encoding: 'utf-8' });
+            console.log(data);
+            var arrayOfLines_1 = data.split('\n');
+            var stringEndOFFile_1 = "" + CodeToIdentifie;
+            var indexWhereLineBreak = 0;
+            arrayOfLines_1.map(function (val, index) {
+                if (arrayOfLines_1[index].trim() == stringEndOFFile_1.trim()) {
+                    indexWhereLineBreak = index;
+                    arrayOfLines_1[index] = ("\n        " +replaceSpecificCharcter("LowerCase"
+                    ,replaceSpecificCharcter("className",stringToAdd,className)
+                    ,className.toLowerCase())
+                     + "\n         " 
+                     + stringEndOFFile_1 
+                     + "\n         ");
+                }
+            });
+            fs_1.writeFileSync(filePath, arrayOfLines_1.join('\n'));
+            console.log(indexWhereLineBreak);
+        
+    };
+    return codeinjector;
+}());
+
+
+
+function replaceSpecificCharcter(symbol, string, changeelement) {
+
+    var tempString = string;
+    if (tempString.indexOf(symbol) > -1) {
+
+        tempString = replaceSpecificCharcter(symbol, string.replace(symbol, changeelement), changeelement);
+
+    }
+    return tempString;
+}
+exports.codeinjector = codeinjector;
---
+++ readJson.js
@@ -0,0 +1,39 @@
+"use strict";
+exports.__esModule = true;
+var fs_1 = require("fs");
+var codeinjectore_1 = require("./codeinjectore");
+fs_1.readFile('./config_1.json', { encoding: 'utf-8' }, function (err, data) {
+    var ci = new codeinjectore_1.codeinjector();
+    if (!err) {
+       // console.log(data);
+        var json = JSON.parse(data);
+       // console.log(json);
+        Object.entries(json).forEach(function (_a) {
+            var key = _a[0], value = _a[1];
+            var filedirectorypath = value["filedirectorypath"];
+            var filename = value["filename"];
+            var className = value["className"];
+            Object.entries(value).forEach(function (_a) {
+                var key = _a[0], value = _a[1];
+               // console.log(value);
+                if(typeof(value)=="object"){
+                ci.main(filename, filedirectorypath, className, value["CodeToChange"], value["CodeToIdentifie"]);
+            }
+            });
+        });
+        // json.forEach(element => {
+        //     console.log(element);
+        // });
+        //console.log("filedirectorypath"+json)
+        //     let arrayOfLines=data.split('\n');
+        //     let stringEndOFFile="//#endregion code region end by zeal";
+        //     var indexWhereLineBreak=0;
+        //     arrayOfLines.map((val,index)=>{
+        // if(arrayOfLines[index].trim()==stringEndOFFile.trim()){
+        //     indexWhereLineBreak=index;
+        //     arrayOfLines[index]=(`
+        //     ${stringToAdd} 
+        //      ${stringEndOFFile}
+        //      `);
+    }
+});
